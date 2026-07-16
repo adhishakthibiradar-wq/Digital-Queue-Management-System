4a3931ebb4907dc339db697a274db90c8521b693
Adishakti Biradar <adhishakthibiradar@gmail.com>
2026-07-16 20:05:40 +0530

    add deployed url

--- server/server.js
+++ server/server.js
@@ -20,7 +20,10 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://digital-queue-management-system-rho.vercel.app",
+    ],
     credentials: true,
   })
 );
